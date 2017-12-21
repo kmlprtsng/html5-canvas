f75c851521c0767139c9726ac2c92d1a488e97b8
Kamalpreet Chauhan <kamalpreet.chauhan@pisolve.com>
2017-12-21 15:08:08 +0000

    add circles with a loop

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -2,6 +2,8 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 10;
 
+//fill with fill the inside of the object with a color
+
 var c = canvas.getContext("2d");
 c.fillStyle = "rgba(255, 0, 0, 0.4)"; //fillStyles for rectangle
 c.fillRect(100, 100, 100, 100); //c.fillRect(x, y, width, height);
@@ -21,7 +23,26 @@ c.stroke(); //for the line to show
 //end angle, how long we would like the arc to go on for
 c.beginPath(); //break away from the previously designed line
 c.strokeStyle = "pink";
+c.fillStyle = "orange";
 c.arc(300, 300, 30, 0, Math.PI * 2, false);
 c.stroke(); //creates the border
-c.fillStyle = "orange";
 c.fill();
+
+for (var i = 0; i < 100; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+
+  c.beginPath(); //break away from the previously designed line
+  c.strokeStyle = getRandomColor();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.stroke(); //creates the border
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
